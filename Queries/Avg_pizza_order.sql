112da0c2db4031a571201b14a26ae5087af0e341
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-09-02 21:53:05 +0530

    Add files via upload

--- Queries/Avg_pizza_order.sql
+++ Queries/Avg_pizza_order.sql
@@ -1,1 +1,5 @@
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
 
+select round(avg(quantity),0) from
+(select sum(order_details.quantity) as quantity, orders.order_date from order_details join
+orders on order_details.order_id=orders.order_id group by order_date) as order_quantity;
